4cf17261a633b48d6832d5a40eb5178635099d40
Jose Armando Elias Zacarias <josearmandoeliaszacarias@gmail.com>
2021-09-19 15:31:50 -0600

    Inicio del proyecto de git 2021

---
+++ Git1997/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Git1997
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio del proyecto!");
+            Console.WriteLine("Proyecto para la administracion de empleados.");
+            Console.WriteLine("-----------MENU--------------");
+            Console.WriteLine("1) Agregar empleados.");
+            Console.WriteLine("2) Lista de empleados.");
+            Console.WriteLine("2) Salir");
+        }
+    }
+}

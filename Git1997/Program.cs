4c7a0a9918396b85db363a2f27f60b8de0787644
Jose Armando Elias Zacarias <josearmandoeliaszacarias@gmail.com>
2021-09-19 16:43:59 -0600

    se agrego una nueva clase empleado al proyecto

--- Git1997/Program.cs
+++ Git1997/Program.cs
@@ -14,4 +14,28 @@ namespace Git1997
             Console.WriteLine("2) Salir");
         }
     }
+    abstract class Empleado
+    {
+        public Empleado()
+        {
+
+        }
+
+        protected Empleado(string tipoEmpleado, string nombre, char genero, double salario)
+        {
+            TipoEmpleado = tipoEmpleado;
+            Nombre = nombre;
+            Genero = genero;
+            Salario = salario;
+        }
+
+        public string TipoEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public char Genero { get; set; }
+        public double Salario { get; set; }
+        public abstract double CalCularSalario();
+
+
+
+    }
 }

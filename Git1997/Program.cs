aef54322a55a65dce3a4e9e53ad9473b0b058cfa
Jose-Armando-Elias-Zacarias <josearmandoeliaszacarias@gmail.com>
2021-12-30 16:44:05 -0600

    correcciones y mensajes para imprimir gerentes.

--- Git1997/Program.cs
+++ Git1997/Program.cs
@@ -51,12 +51,14 @@ namespace Git1997
                     Console.WriteLine("------List de Empleados de la empresa-----");
                     foreach (var ger in gerentes)
                     {
-                        Console.WriteLine("Datos del gerente:");
+                        Console.WriteLine("Datos del Gerente:");
                         Console.WriteLine($"\tNombre: {ger.Nombre}");
                         Console.WriteLine($"\tTipo gerente: {ger.TipoEmpleado}");
                         Console.WriteLine($"\tPuesto: {ger.Genero}");
                         Console.WriteLine($"\tSalario: {ger.CalCularSalario()}");
+                        Console.WriteLine("--------------------------------------------------------");
                         Console.WriteLine();
+
                     }
                     break;
                 case '3':

d5309ffcffe3855d1ad05e7e918bda9c7bdca4e7
Jose-Armando-Elias-Zacarias <josearmandoeliaszacarias@gmail.com>
2022-01-03 22:51:02 -0600

    2022

---
+++ Git1997/Secretaria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git1997
+{
+    class Secretaria : Empleado
+    { 
+        public Secretaria()
+        {
+
+        }
+
+        public Secretaria(string tipoSecretaria, string area)
+        {
+            TipoSecretaria = tipoSecretaria;
+            Area = area;
+        }
+
+        public String TipoSecretaria { get; set; }
+        public String Area { get; set; }
+
+        public override double CalCularSalario()
+        {
+            return base.Salario * 1.05;
+        }
+    }
+}
